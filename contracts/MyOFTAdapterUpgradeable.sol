b97f7b8d39cdfa03c51bd8481e6aa65e25981bae
turingczz <920886811@163.com>
2025-03-12 09:20:01 +0800

    feat: compile MyOFTAdapterUpgradeable ok

--- contracts/MyOFTAdapterUpgradeable.sol
+++ contracts/MyOFTAdapterUpgradeable.sol
@@ -1,9 +1,26 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.22;
 
+import { RateLimiter } from "@layerzerolabs/oapp-evm/contracts/oapp/utils/RateLimiter.sol";
 import { OFTAdapterUpgradeable } from "@layerzerolabs/oft-evm-upgradeable/contracts/oft/OFTAdapterUpgradeable.sol";
 
-contract MyOFTAdapterUpgradeable is OFTAdapterUpgradeable {
+contract MyOFTAdapterUpgradeable is OFTAdapterUpgradeable, RateLimiter {
+    string public constant version = "1.0.0";
+
+    address public pauseAdmin;
+    bool public paused;
+
+    event PauseAdminChanged(
+        address adminSetter,
+        address oldAddress,
+        address newAddress
+    );
+
+    event PauseEvent(
+        address pauseAdmin,
+        bool paused
+    );
+
     constructor(address _token, address _lzEndpoint) OFTAdapterUpgradeable(_token, _lzEndpoint) {
         _disableInitializers();
     }
@@ -11,5 +28,57 @@ contract MyOFTAdapterUpgradeable is OFTAdapterUpgradeable {
     function initialize(address _delegate) public initializer {
         __OFTAdapter_init(_delegate);
         __Ownable_init(_delegate);
+    }
+
+    // cross out: check rateLimit and pause
+    function _debit(
+        address _from,
+        uint256 _amountLD,
+        uint256 _minAmountLD,
+        uint32 _dstEid
+    ) internal override(OFTAdapterUpgradeable) whenNotPaused returns (uint256 amountSentLD, uint256 amountReceivedLD) {
+        _outflow(_dstEid, _amountLD);
+        (amountSentLD, amountReceivedLD) = super._debit(_from, _amountLD, _minAmountLD, _dstEid);
+    }
+
+    // cross in: check pause
+    function _credit(
+        address _to,
+        uint256 _amountLD,
+        uint32 _srcEid /*_srcEid*/
+    ) internal virtual override(OFTAdapterUpgradeable) whenNotPaused returns (uint256 amountReceivedLD) {
+        (amountReceivedLD) = super._credit(_to, _amountLD, _srcEid);
+    }
+
+    /**
+     * @dev Sets the rate limits based on RateLimitConfig array. Only callable by the owner or the rate limiter.
+     * @param _rateLimitConfigs An array of RateLimitConfig structures defining the rate limits.
+     */
+    function setRateLimits(RateLimitConfig[] calldata _rateLimitConfigs) external onlyOwner {
+        _setRateLimits(_rateLimitConfigs);
+    }
+
+    // pause ...
+    function setPauseAdmin(address _account) public onlyOwner {
+        require(_account != address (0), "invalid _account");
+        address oldPauseAdmin = pauseAdmin;
+        pauseAdmin = _account;
+        emit PauseAdminChanged(msg.sender, oldPauseAdmin, pauseAdmin);
+    }
+
+    modifier whenNotPaused() {
+        require(!paused, "pause is on");
+        _;
+    }
+
+    function pause() public whenNotPaused {
+        require(msg.sender == pauseAdmin, "Illegal pause permissions");
+        paused = true;
+        emit PauseEvent(msg.sender, paused);
+    }
+
+    function unpause() public onlyOwner {
+        paused = false;
+        emit PauseEvent(msg.sender, paused);
     }
 }
